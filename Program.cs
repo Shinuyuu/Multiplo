1aadb7d9b9895cc8075cb61be1496a80f81d48e7
= <=>
2023-05-07 13:17:52 -0300

    sua vida seria um multiplo da sua alma?

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿Console.ForegroundColor = ConsoleColor.White;
+string frase = "- - Multiplo - -\n";
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(55);
+}
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+frase = "Escreva um número para se dividir: ";
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(55);
+}
+int numero = Convert.ToInt32(Console.ReadLine());
+Console.ForegroundColor = ConsoleColor.Cyan;
+frase = "Escreva um número para dividir o numero escolhido: ";
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(55);
+}
+int numero2 = Convert.ToInt32(Console.ReadLine());
+if ((numero % numero2) < 1)
+{
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+frase = $"O número {numero} é multiplo com o número {numero2}\n";
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(55);
+}
+}
+else if ((numero % numero2) >= 1)
+{
+Console.ForegroundColor = ConsoleColor.Red;
+frase = $"O número {numero} não é multiplo com o número {numero2}\n";
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(55);
+}
+}
+Console.ResetColor();
